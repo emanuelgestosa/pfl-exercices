04224c3a8921654d698b537ceb7f012159d4e82d
Emanuel Gestosa <emanuel-367@hotmail.com>
2022-10-07 21:44:53 +0100

    3.8 fix words with several spaces between them

--- f3/3_8.hs
+++ f3/3_8.hs
@@ -2,7 +2,7 @@ palavras :: String -> [String]
 palavras = foldr (\c n->
     case n of
       []->if c/=' ' then [[c]] else [[]]
-      x:xs->if c/=' ' then (c:x):xs else []:x:xs
+      x:xs->if c/=' ' then (c:x):xs else if x/=[] then []:x:xs else x:xs
     ) []
 
 despalavras :: [String] -> String
